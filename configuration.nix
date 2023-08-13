c5df84b6d1767621ec48bf85327e283dcd68530e
Robbin Bonthond <robbin@bonthond.com>
2023-08-13 13:41:24 -0500

    updated

--- configuration.nix
+++ configuration.nix
@@ -3,6 +3,7 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   config,
+  inputs,
   pkgs,
   ...
 }: {
@@ -10,15 +11,33 @@
     ./hardware-configuration.nix
   ];
 
-  # Bootloader.
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
+
+  # Enable flakes
+  nix.package = pkgs.nixVersions.unstable;
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
-
-  # kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.tmp.cleanOnBoot = true;
 
+  # Security
+  nix.settings.trusted-users = ["root" "robbin"];
+  security.sudo.wheelNeedsPassword = false;
+
+  # Networking
   networking.hostName = "nixos"; # Define your hostname.
   #networking.wireless = {
   #  enable = true;
@@ -37,23 +56,102 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
   # Set your time zone.
   time.timeZone = "America/Chicago";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.utf8";
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # enable opengl support
+  hardware.opengl.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.robbin = {
+    isNormalUser = true;
+    description = "Robbin Bonthond";
+    extraGroups = ["networkmanager" "wheel"];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget
+    curl
+    vim
+    git
+    autorandr
+    alejandra
+  ];
+
+  # Enable sound
+  sound.enable = true;
+  nixpkgs.config.pulseaudio = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+  services.printing.drivers = [pkgs.cnijfilter2];
+  services.avahi.enable = true;
+  services.avahi.nssmdns = true;
+
+  # Virtualbox
+  #virtualisation.virtualbox.host.enable = true;
+  #virtualisation.virtualbox.host.enableExtensionPack = true;
+  #users.extraGroups.vboxusers.members = [ "robbin" ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver = {
-    displayManager.sddm.enable = true;
-    desktopManager.plasma5.enable = true;
+    enable = true;
+    libinput.enable = true;
+    layout = "us";
+    xkbVariant = "";
+    displayManager = {
+      sddm.enable = true;
+      #autoLogin = {
+      #  enable = true;
+      #  user = "robbin";
+      #};
+    };
+    desktopManager.plasma5 = {
+      enable = true;
+      useQtScaling = true;
+    };
   };
+  security.pam.services = {
+    sddm.enableKwallet = true;
+  };
+  #environment.systemPackages = with pkgs; [
+  #  kdeFrameworks.kwallet
+  #  kdeapplications.kwalletmanager
+  #];
 
-  # Enable XFCE Desigkpo Environment.
+  # Enable XFCE Desktop Environment.
   #services.xserver = {
+  #  enable = true;
   #  desktopManager = {
   #    xterm.enable = true;
   #    xfce.enable = true;
@@ -68,103 +166,21 @@
   #  fadeDelta = 4;
   #};
 
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-  services.printing.drivers = [pkgs.cnijfilter2];
-  services.avahi.enable = true;
-  services.avahi.nssmdns = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.robbin = {
-    isNormalUser = true;
-    description = "Robbin Bonthond";
-    extraGroups = ["networkmanager" "wheel"];
-  };
-  nix.settings.trusted-users = ["root" "robbin"];
-
-  security.sudo.wheelNeedsPassword = false;
-
-  # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "robbin";
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # Enable flakes
-  nix = {
-    package = pkgs.nixVersions.stable;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    wget
-    vim
-    git
-    autorandr
-    alejandra
-  ];
-
-  # Virtualbox
-  #virtualisation.virtualbox.host.enable = true;
-  #virtualisation.virtualbox.host.enableExtensionPack = true;
-  # users.extraGroups.vboxusers.members = [ "robbin" ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.05"; # Did you read the comment?
-
-  hardware.opengl.enable = true;
+  # hyprland
+  #nix.settings = {
+  #  substituters = ["https://hyprland.cachix.org"];
+  #  trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  #};
+  #services.xserver = {
+  #  enable = true;
+  #  displayManager.gdm = {
+  #    enable = true;
+  #    #wayland = true;
+  #  };
+  #};
+  #programs.hyprland = {
+  #  enable = true;
+  #  #xwayland.enable = true;
+  #  package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+  #};
 }
--- flake.nix
+++ flake.nix
@@ -3,14 +3,17 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    #hyprland.url = "github:hyprwm/Hyprland";
   };
   outputs = inputs @ {
     self,
     nixpkgs,
     nixos-hardware,
+    #hyprland,
   }: {
     nixosConfigurations = {
       "nixos" = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
